cb2c11eb993e38d885d8b0db215e8483dc8abb84
unknown <cba5530@gmail.com>
2020-12-27 22:17:03 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 //require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose')
-const db = mongoose.connection
 const bodyParser = require('body-parser')
 const hdb = require('handlebars')
 const methodOverride = require('method-override') 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 hdb.registerHelper('ifEquals', function (arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
@@ -19,14 +16,6 @@ hdb.registerHelper('ifEquals', function (arg1, arg2, options) {
 //require express-handlebars here
 const exphbs = require('express-handlebars')
 
-
-db.on('error', () => {
-  console.log('mongdb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
