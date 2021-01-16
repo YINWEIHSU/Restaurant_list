301a102b249408fede839064188739a1f8a4eb7c
YinWei <cba5530@gmail.com>
2021-01-16 23:53:17 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const express = require('express')
 const router = express.Router()
 //準備引入路由模組
 const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
+
+
+router.use('/users', users)
+router.use('/restaurants', restaurants)
 router.use('/', home)
 
-const restaurants = require('./modules/restaurants')
-router.use('/restaurants', restaurants)
+
 //匯出路由器
 module.exports = router
